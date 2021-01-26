d9ddab8f05b21293e68c53387884910e62fcf3fe
cybrpete <16666845+cybrpete@users.noreply.github.com>
2021-01-26 19:25:46 +1100

    Add files via upload

---
+++ LinkedInLinks.js
@@ -0,0 +1,32 @@
+/* This script is designed for the browser console, it pulls all LinkedIn
+links from the current tab and outputs them to a new popup window */
+
+var links = document.querySelectorAll("a"); // Extract all links in current tab.
+var lnk_str = [];
+
+// Cleans links and pushes hyperlinks into lnk_str array.
+var i;
+var hyper;
+for (i=0; i<links.length; i+=1) {
+  hyper = links[i].href;
+  lnk_str.push([hyper]);
+}
+
+// Function to show links in a separate tab if they match the search query.
+function show_links() {
+  // Seeding table with header.
+  var table = "<table><thead><th>LinkedIn Links</th></thead><tbody>";
+  // Processing of all links in lnk_str array.
+  var j;
+  for (j=0; j<lnk_str.length; j+=1) {
+    // Only adding search string matches to the table. Change linkedin to
+    // string of choice.
+    if (lnk_str[j][0].match("linkedin")!==null) {
+      table += "<tr><td>"+ (lnk_str[j][0]).link(lnk_str[j][0]) +"</td></tr>";
+    }
+  }
+  // New window generation and table display.
+  var output = window.open("");
+  output.document.write(table);
+}
+show_links();
